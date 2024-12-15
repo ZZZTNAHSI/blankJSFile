54cc0b2eded56311763f073e00c118470630c9ac
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-12-14 22:34:28 -0600

    New proj!

--- reactTomfoolery/11-starting-project/01-starting-project/src/App.jsx
+++ reactTomfoolery/11-starting-project/01-starting-project/src/App.jsx
@@ -6,13 +6,13 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1} />
-        <TimerChallenge title="Not Easy" targetTime={5} />
-        <TimerChallenge title="Getting Tough" targetTime={10} />
-        <TimerChallenge title="Pros Only" targetTime={15} />
+        <TimerChallenge title="Piss Easy" targetTime={1} />
+        <TimerChallenge title="Eh" targetTime={5} />
+        <TimerChallenge title="Alright" targetTime={10} />
+        <TimerChallenge title="Sigma" targetTime={15} />
       </div>
     </>
   );
 }
 
-export default App;
+export default App; 
