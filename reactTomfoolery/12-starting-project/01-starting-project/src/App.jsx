9f4a61ba5da8e59161ca3179a2208b7273b2cbc7
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-12-20 22:52:38 -0600

    finsish the title for the black thing and the col where the todo lists will go, still need to determine how to render said todo lists

--- reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
+++ reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
@@ -3,8 +3,12 @@ function App() {
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
       <div className="flex flex-row w-screen">
-        <div className="flex flex-col bg-black w-[30%] s-12 h-screen rounded-tr-3xl rounded-br-3xl min-w-[250px] max-w-[450px]">
-          <p className="color">YOUR PROJECTS</p>
+        <div className="flex flex-col flex-wrap pl-12 bg-black w-[35%] s-12 h-screen rounded-tr-3xl rounded-br-3xl min-w-[300px] max-w-[450px]">
+          <p className="text-white text-2xl font-semibold mt-[70px]">YOUR PROJECTS</p>
+          <button className="bg-stone-800 mb-8 text-stone-400 max-w-40 rounded-[5px] min-h-[40px] text-lg mt-10">+ Add Project</button>
+          <div className="flex flex-col ">
+
+          </div>
         </div>
       </div>
     </>
