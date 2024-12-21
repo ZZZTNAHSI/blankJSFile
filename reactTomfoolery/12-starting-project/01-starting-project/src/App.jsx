bf10e8a8e9520883fd82a6553bd2cc725c177bb8
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-12-20 22:01:31 -0600

    tailwind a whore 2

--- reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
+++ reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <div className="flex flex-row w-screen">
+        <div className="flex flex-col bg-black w-[30%] s-12 h-screen rounded-tr-3xl rounded-br-3xl min-w-[250px] max-w-[450px]">
+          <p className="color">YOUR PROJECTS</p>
+        </div>
+      </div>
     </>
   );
 }
