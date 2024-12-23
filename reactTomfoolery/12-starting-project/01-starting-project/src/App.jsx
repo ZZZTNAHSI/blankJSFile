104cf94be5783b7ffcb8fdc7a21f51ad42b2555e
ZZZTnahsi <Ishantmahey1@gmail.com>
2024-12-22 18:30:11 -0600

    Turning point, no going back, not using imperativehandle and instead just using state

--- reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
+++ reactTomfoolery/12-starting-project/01-starting-project/src/App.jsx
@@ -2,23 +2,46 @@ import NewProj from "./components/NewProj";
 import Button from "./components/Button";
 import NoProj from "./components/NoProj";
 import TodoList from "./components/TodoList";
+import {useState, useRef } from "react";
 
 
 function App() {
+  const [masterTable, setMasterTable] = useState([]);
+  const [submitted, setSubmitted] = useState(false);
+
+  const info = useRef();
+
+  function handleSubmit() {
+    setSubmitted(true);
+  }
+
+  function handleSubmit2(title, desc, dueDate) {
+    setMasterTable((prev) => {
+      return [...prev, {
+        title: title.current.value,
+        desc: desc.current.value,
+        dueDate: dueDate.current.value,
+        tasks: []
+      }];
+    });
+    console.log("This is master: " + masterTable);
+    setSubmitted((prev) => !prev);
+  }
+
   return (
     <>
       
       <div className="flex flex-row w-screen mt-16">
         <div className="flex flex-col flex-wrap pl-12 bg-black w-[35%] s-12 h-screen rounded-tr-3xl rounded-br-3xl min-w-[300px] max-w-[450px]">
           <p className="text-white text-2xl font-semibold mt-[70px]">YOUR PROJECTS</p>
-          <Button title="+ Add Project" size="500" />
+          <Button title="+ Add Project" />
           <div className="flex flex-col ">
               <button className="text-stone-400 bg-black rounded-[5px] min-h-[30px] p-2 text-md max-w-[100%] mr-12 text-left mb-1 hover:bg-stone-800">TEMP NAME</button>
           </div>
         </div>
-        <TodoList />
+        {/* <TodoList /> */}
         {/* <NoProj /> */}
-        {/* <NewProj /> */}
+        <NewProj handleSubmit={handleSubmit} handleSubmit2={handleSubmit2} ref={info}/>
       </div>
     </>
   );
--- reactTomfoolery/12-starting-project/01-starting-project/src/components/NewProj.jsx
+++ reactTomfoolery/12-starting-project/01-starting-project/src/components/NewProj.jsx
@@ -1,14 +1,39 @@
-import NewProjInput from "./NewProjInput"
+import { forwardRef, useRef, useImperativeHandle} from "react";
 
-export default function NewProj() {
-    return <form className="flex flex-col justify-items-center basis-full ml-10 mr-[20%] mt-20 w-[40%]">
+const NewProj = forwardRef(function NewProj({handleSubmit, handleSubmit2}, ref) {
+    const title = useRef();
+    const desc = useRef();
+    const dueDate = useRef();
+
+    useImperativeHandle(ref, () => {return {
+        getTitle: () => {return title.current.value},
+        getDesc: () => {return desc.current.value},
+        getDueDate: () => {return dueDate.current.value}
+    }});
+    
+
+    
+    return <form className="flex flex-col justify-items-center basis-full ml-10 mr-[20%] mt-20 w-[40%]" onSubmit={(e) => {
+        handleSubmit();
+        handleSubmit2(title, desc,dueDate);
+        e.preventDefault();
+        title.current.value = "";
+        desc.current.value = "";
+        dueDate.current.value = "";
+
+        }}>
         <div className="flex flex-row-reverse ">
-            <button className="rounded-lg text-white bg-black  ml-5 min-w-[90px] min-h-10 ">Save</button>
-            <button className=" min-w-[90px] rounded-lg min-h-10 text-black hover:bg-stone-400">Cancel</button>
+            <button type="submit" className="rounded-lg text-white bg-black  ml-5 min-w-[90px] min-h-10 ">Save</button>
+            <button type="button" className=" min-w-[90px] rounded-lg min-h-10 text-black hover:bg-stone-400">Cancel</button>
         </div>
-        <NewProjInput title="Title" placeHolder="" height={4}/>
-        <NewProjInput title="Description" placeHolder="" height={22}/>
-        <NewProjInput title="Due Date" placeHolder="DD.MM.YYYY" height={6}/>
+        <label className="text-stone-500 min-h-[20px] font-bold min-w-full mt-4">Title</label>
+        <textarea ref={title} className={"resize-none bg-stone-200 rounded-sm outline-none outline p-1 focus:border-b-black border-b-stone-400 border-2 h-16"}></textarea>
+        <label className="text-stone-500 min-h-[20px] font-bold min-w-full mt-4">Description</label>
+        <textarea ref={desc} className={"resize-none bg-stone-200 rounded-sm outline-none outline p-1 focus:border-b-black border-b-stone-400 border-2 h-24"}></textarea>
+        <label className="text-stone-500 min-h-[20px] font-bold min-w-full mt-4">Due Date</label>
+        <textarea ref={dueDate} placeholder="DD.MM.YYYY" className={"resize-none bg-stone-200 rounded-sm outline-none outline p-1 focus:border-b-black border-b-stone-400 border-2 h-16"}></textarea>
 
     </form>
-}
+});
+
+export default NewProj;
