db22cb2c612d81c4597b8ce8ddd41c9aee8b0e43
ZZZTnahsi <Ishantmahey1@gmail.com>
2025-01-19 22:50:54 -0600

    erm what the fuck

--- reactTomfoolery/18-starting-project/01-starting-project/src/App.jsx
+++ reactTomfoolery/18-starting-project/01-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback,  } from 'react';
 
 import Places from './components/Places.jsx';
 import Modal from './components/Modal.jsx';
@@ -7,7 +7,7 @@ import logoImg from './assets/logo.png';
 import AvailablePlaces from './components/AvailablePlaces.jsx';
 import Error from './components/Error.jsx';
 import { updateUserPlaces, fetchUserPlaces } from './http.js';
-
+import { useFetch } from './hooks/useFetch.js';
 
 function App() {
   const selectedPlace = useRef();
@@ -15,26 +15,9 @@ function App() {
   
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
+
+  const {isFetching, error, fetchedData: userPlaces, setFetchData: setUserPlaces} = useFetch(fetchUserPlaces, []);
   
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
-
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({message: error.message || "faield to fetch user places."});
-      }
-      setIsFetching(false);
-    }
-    fetchPlaces();
-
-  }, [])
-
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
     selectedPlace.current = place;
@@ -74,7 +57,7 @@ function App() {
     }
 
     setModalIsOpen(false);
-  }, [userPlaces]);
+  }, [userPlaces, setUserPlaces]);
 
   function handleError() {
     setErrorUpdatingPlaces(null);
