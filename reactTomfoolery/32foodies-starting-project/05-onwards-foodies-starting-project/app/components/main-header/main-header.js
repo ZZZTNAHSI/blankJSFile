1cdc0aa8cc3808a2e66be2707fa5442a0fd43b89
ZZZTnahsi <Ishantmahey1@gmail.com>
2025-03-30 22:33:31 -0500

    sigh

--- reactTomfoolery/32foodies-starting-project/05-onwards-foodies-starting-project/app/components/main-header/main-header.js
+++ reactTomfoolery/32foodies-starting-project/05-onwards-foodies-starting-project/app/components/main-header/main-header.js
@@ -3,6 +3,8 @@ import Link from "next/link";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+
 export default function MainHeader() {
     return (<>
     <MainHeaderBackground />
@@ -13,8 +15,8 @@ export default function MainHeader() {
         </Link>
         <nav className={classes.nav}>
             <ul>
-                <li><Link href="/meals">Browse Meals</Link></li>
-                <li><Link href="/community">Foodies Community</Link></li>
+                <li><NavLink href="/meals">Browse Meals</NavLink></li>
+                <li><NavLink href="/community">Foodies Community</NavLink></li>
             </ul>
         </nav>
     </header>
