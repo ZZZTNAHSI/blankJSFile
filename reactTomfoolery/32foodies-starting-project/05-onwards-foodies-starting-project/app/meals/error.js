bf101f9a11e3da97df8a09a7b6748905544b961c
ZZZTnahsi <Ishantmahey1@gmail.com>
2025-04-06 22:05:16 -0500

    erasdjfallsdfjsadf

--- reactTomfoolery/32foodies-starting-project/05-onwards-foodies-starting-project/app/meals/error.js
+++ reactTomfoolery/32foodies-starting-project/05-onwards-foodies-starting-project/app/meals/error.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
+
 'use client';
 
 export default function Error({error}) {
     return <main className="error">
         <h1>An error occured</h1>
         <p>Failed to fetch meal data Please try again later. </p>
-=======
-export default function Error() {
-    return <main>
-        
->>>>>>> c0275637a0681f3fcf190daeb1fd43577fb35a76
     </main>
 }
